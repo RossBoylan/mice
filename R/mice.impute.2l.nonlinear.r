ab25630b872801146b5aaca19438bd6be024abd3
Ross Boylan <ross@biostat.ucsf.edu>
2013-05-03 11:58:08 -0700

    further work on mixed level logistic
    This code seemed reasonable on rerread; otherwise untestd.
    All the work is from yesterday.

--- R/mice.impute.2l.nonlinear.r
+++ R/mice.impute.2l.nonlinear.r
@@ -1,3 +1,5 @@
+require("plyr")
+
 ## exploratory functions to get posterior likelihoods given a linear predictor
 ## that is normal
 ## y is a vector of 0/1 outcomes.
@@ -134,39 +136,32 @@ mice.impute.2l.logit <- function(y, ry, x, type, intercept=TRUE, ...)
   return(y[!ry])
 }
 
-# mixed level 1 and 2 predictors of outcomes
 mice.impute.2lmixed.logit <- function(y, ry, x, type, intercept=TRUE, ...)
 {
+    ## mixed level 1 and 2 predictors of outcomes
+    ## variables with 2 at end of name are level 2, one obs /cluster
+
     ## We make two additions to the input data.
     ## We assume there is an unobserved variable z with
     ## Pr(y=1) = 1/(1+exp(-z))
-    ## Second, for each group there is a continuous group effect z2
-    ## with mean 0 and variance tauz
-    ## It seems likely that tau and tauz may be correlated, slowing convergence.
-    ## z=Xb+z2
-    ## Define zres= z-Xb
+    ## Second, for each group there is a continuous group effect theta2
+    ## with mean 0 and sd tau
+    ## z=Xb+theta+e
+    ## (theta is theta2 duplicated for each observation at level 1)
+    ## e is normal error with sd sigma
+    ## It seems likely that sigma and tau may be correlated, slowing convergence.
   ## Initialize
   #n.iter <- 100
   n.iter <- 1000
   nry <- !ry
+  nmiss <- sum(nry)
   n.class <- length(unique(x[, type==(-2)]))
   gf.full <- factor(x[,type==(-2)], labels=1:n.class)
   ids <- contract(data.frame(gf.full), gf.full)
+  nids <- aaply(gf.full, gf.full, length)
   gf <- gf.full[ry]
 
   X <- as.matrix(x[,type>0])
-
-  # level 1 latent variables
-  # establish initial guesses with Laplace's law of succession
-  p <- (y+1)/3
-  # set missing values to the sample average
-  p[is.na(p)] <- mean(y, na.rm=TRUE)
-  # invert to get the latent variable
-  z <- qlogis(p)
-  zres <- z-mean(z)  # inital guess assumes all coefficient except intercept are 0
-
-  # at this point we have imputed values for the latent z
-  # but not for the y
 
   # compute some constants for the loop
   xtx <- t(X) %*% X
@@ -175,63 +170,95 @@ mice.impute.2lmixed.logit <- function(y, ry, x, type, intercept=TRUE, ...)
   if (length(pen)==1) pen <- matrix(pen)
   v <- solve(xtx + diag(pen))
 
+  # level 1 latent variables
+  # establish initial guesses with Laplace's law of succession
+  p <- (y+1)/3
+  # set missing values to the sample average
+  # I think they y have already been imputed before entering this function
+  p[is.na(p)] <- mean(y, na.rm=TRUE)
+  # invert to get the latent variable
+  z <- qlogis(p)
+
+  # at this point we have imputed values for the latent z
+  # but not for the y.  I think it's unnecessary, but just in case...
+  y[nry] <- sample(y[ry], nmiss, replace=TRUE)
+
   # continue setting initial values
-  coef <- t(z %*% X %*% v)
-  # for each iteration record sigma2, coef, beta, mu2, y2
+
+  beta.post.mean <- t(z %*% X %*% v)
+  beta <- beta.post.mean
+  resid <- z-X%*%beta
+  # level 2 latent variables initial values
+  theta2 <- aaply(resid, gf.full, mean)
+  theta <- unlist(mapply(rep, theta2, nids))
+
+  # No initial values needed
+  tau <- NA_real
+  sigma <- NA_real
+  z.prior.mean = rep(NA_real, nrow(X))
+
+
+# for each iteration record sigma2, beta.post.mean, beta, mu2, y2
   nvar <- ncol(v)
-  ntrace <- 1+nvar+nvar+n.class+n.class
+  ntrace <- 2+nvar+n.class+nrow(X)+nmiss+nvar+nrow(X)
   MCTRACE <<- matrix(NA_real_, nrow=n.iter+1, ncol= ntrace)
-  MCTRACE[1, seq(ntrace-n.class+1, ntrace)] <<- y2
+  # order is all the posterior values and then some related stats
+  MCTRACE[1,] <<- c(tau, sigma, beta, theta2, z, y[nry], beta.post.mean, z.prior.mean)
 
   for (iter in 1:n.iter){
       # X already has an intercept in it
 
-      # WORK IN PROGRESS
+      # draw posterior values for tau given all other values
+      # theta2 ~ Norm(0, tau)
+      tau <- sqrt(sum(theta2^2)/rchisq(1, n.class-1))
 
-      # draw values for z2 given all other values
-      theta2 <- c(by(y, gf.full, mean))
-      theta2e <- expand(theta2, gf.full,
+      # and for sigma, also known mean 0
+      resid <- z- X %*% beta - theta
+      sigma <- sqrt(sum(resid^2)/rchisq(1, nrow(X)))
+      # the use of df here and df-1 above is deliberate
+      # reflecting different prior distns needed for proper posterior
 
-      # the next section is modeled on .norm.draw except
-      # it estimates the model from all the data
-      # and then imputes all the data.
-      # Also, this works on the level 2 data.
+      # draw posterior theta
+      thetapost <- adply(z - X %*%beta.post.mean, gf.full, function(x) {
+          n <- length(x)
+          varpost <- 1/(1/tau^2 + n/sigma^2)
+          mupost <- sum(x)/sigma^2/varpost
+          data.frame(mu=mupost, var=varpost)
+      })
+      theta2 <- rnorm(n.class, mean=thetapost$mu,
+                      sd=sqrt(thetapost$var))
+      theta <- unlist(mapply(rep, theta2, nids))
 
-      coef <- t(y2 %*% X %*% v)
-      residuals <- y2 - X %*% coef
-      sigma2 <- sqrt(sum((residuals)^2)/rchisq(1, nrow(X) - ncol(X)))  # SvB 01/02/2011
-      beta <- coef + (t(chol((v + t(v))/2)) %*% rnorm(ncol(X))) * sigma2
+      ## draw posterior beta and redraw sigma
+      w <- z-theta
+      beta.post.mean <- t(w %*% X %*% v)
+      residuals <- z - X %*% beta.post.mean - theta
+      sigma <- sqrt(sum((residuals)^2)/rchisq(1, nrow(X) - ncol(X)))  # SvB 01/02/2011
+      beta <- beta.post.mean + (t(chol((v + t(v))/2)) %*% rnorm(ncol(X))) * sigma
 
-      # mu2 is center of prior dist of y2 given the coefficient draws
-      mu2 <- X %*% beta
+      # z.prior.mean is center of prior dist of z given the coefficient draws
+      z.prior.mean <- X %*% beta + theta
 
-      # Prior y2 ~ N(mu2, sigma2)  Note sigma2 is 2nd level, not squared
-      # We do not consider the uncertainty in mu2 and sigma2
-      # commence computation of posterior y2
-      # TODO: stop using column indices to refer to values in the argument of posterior
-      count2 <- t(simplify2array(by(y, gf.full, function(b) c(sum(b, na.rm=TRUE), sum(!is.na(b))))))
-      # draw a posterior y for each group
-      # Numerically intensive
-      posterior <- function(x) {
-          nyes <- x[1]
-          nno <- x[2]-nyes
-          mu <- x[3]
-          grid <- seq(mu-3*sigma2, mu+3*sigma2, length.out=500)
-          p <- 1/(1+exp(-grid))
-          post <- (nyes*log(p)+nno*log(1-p))+log(dnorm(grid, mean=mu, sd=sigma2))
+      # Prior z ~ N(z.prior.mean, sigma)
+      # commence computation of posterior (z|y and all other values)
+      # done numerically
+
+      posterior <- function(x){
+          mu <- x["z.prior.mean"]
+          sigma <- x["sigma"]
+          y <- x["y"]
+          grid <- seq(mu-3*sigma, mu+3*sigma, length=500)
+          p = 1/(1+exp(-grid))
+          if ( y == 0)
+              p = 1- p
+          post <- log(p) + log(dnorm(grid, mean=mu, sd=sigma))
           post <- post-max(post)
           sample(grid, 1, prob=exp(post))
       }
-      # impute all the  posterior ys
-      y2 <- apply(cbind(count2, mu2), 1, posterior)
-      p2 <- 1/(1+exp(-y2))
-
-      # apply p2 to impute y for each missing value
-      # these are indices into p2 from all the missing observations in y
-      into2 <- match(gf.full[nry], id2)
-      y[nry] <- rbinom(sum(nry), 1, p2[into2])
-                                        # for each iteration record sigma2, coef, beta, mu2, y2
-      MCTRACE[iter+1,] <<- c(sigma2, coef, beta, mu2, y2)
+      z <- apply(cbind(z.prior.mean, sigma, y), 1, posterior)
+      # and impute the missing observed values
+      y[nry] <- rbinom(nmiss, 1, 1/(1+exp(-z[nry])))
+      MCTRACE[iter+1,] <<- c(tau, sigma, beta, theta2, z, y[nry], beta.post.mean, z.prior.mean)
   }
   return(y[!ry])
 }

15605dd12798f216fce522c69a5ef8b0ba56f570
Ross Boylan <ross@biostat.ucsf.edu>
2013-06-13 19:07:32 -0700

    nonlinear: use ln(prob) for Metropolis
    Verified that gives same result as before. and computes same  ratios.
    Getting more clever about handling y=0/1 and its probability (just
    change z to -z for computations).

--- R/mice.impute.2l.nonlinear.r
+++ R/mice.impute.2l.nonlinear.r
@@ -253,12 +253,15 @@ mice.impute.2lmixed.logit <- function(y, ry, x, type, intercept=TRUE, ...)
       a <- rnorm(alen, mean=z, sd = max(1.0, sqrt(sigma^2+tau^2)))
       # compute posterior prob for both new and old values
       zboth <- c(a, z)
-      logitprob <- 1/(1+exp(-zboth))
-      logitprob <- ifelse(rep(y==1, 2), logitprob, 1-logitprob)
-      priorprob <- dnorm(zboth, mean=z.prior.mean, sd=sigma)
-      prob <- logitprob*priorprob
-      probr <- prob[seq(alen)]/prob[(alen+1) : (2*alen)]
-      accept <- runif(alen) <= probr
+      # change sign of zboth if y==0 to compute correct prob of outcome
+      yz <- (-1 + 2*y)*zboth
+      # now 1/(1+exp(-yz)) is prob observed outcome for y=0 or 1
+      logitLL <- -log(1+exp(-yz))
+      # Note: zboth, not yz, in next term.
+      priorLL <- dnorm(zboth, mean=z.prior.mean, sd=sigma, log=TRUE)
+      LL <- logitLL+priorLL
+      LLR <- LL[seq(alen)]-LL[(alen+1) : (2*alen)]
+      accept <- runif(alen) <= exp(LLR)
       z[accept] <- a[accept]
 
       # and impute the missing observed values

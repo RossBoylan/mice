401a5ac0e21c7181a5d9104df85ee6b54a57fba6
Ross Boylan <ross@biostat.ucsf.edu>
2013-07-04 11:41:05 -0700

    epsilon .01 only.  worse This is without a range, roughly mid-point of previous one. > effectiveSize(MCTRACE[,1:11])   intrcpt       age   rac_gay nPartners  ethnic.1  ethnic.2     HIV.1     HIV.2  2.439692  9.411273  3.407702 71.372604 14.448664 50.737925  9.281562  6.758827       tau    theta1    theta2  3.249881 14.571320 10.736279

--- R/mice.impute.2l.nonlinear.r
+++ R/mice.impute.2l.nonlinear.r
@@ -413,7 +413,8 @@ mice.impute.2lmixed.logit <- function(y, ry, x, type, intercept=TRUE, ...)
   #weights <- d2a^2
   #weights <- 1.0
   
-  epsilon <- c(0.01, 0.04)/2
+  #epsilon <- c(0.01, 0.04)/2
+  epsilon <- 0.01
   LFsteps <- 20
   r <- HybridMC::hybridMC(y.start=c(beta, tau, theta2), n.samp=100,
                           logDens=logDens, dLogDens=dLogDens, epsilon=epsilon,
